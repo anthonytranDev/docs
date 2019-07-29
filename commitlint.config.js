e115e8743906ffd37eb070b7ce9e9927808b4e8b
anthonytranDev <anthonytran.dev@gmail.com>
2019-07-29 23:21:32 +0100

    chore: commitlint config init

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,15 +1,18 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    'header-max-length': async () => [0, 'always', 72],
     'subject-min-length': async () => [0, 'always', 10],
     'subject-case': async () => [
-      'lower-case', // lowercase
-      'upper-case', // UPPERCASE
-      'camel-case', // camelCase
-      'pascal-case', // PascalCase
-      'sentence-case', // Sentence case
-      'start-case', // Start Case
+      0,
+      'always',
+      [
+        ['lower-case'], // lowercase
+        'upper-case', // UPPERCASE
+        'camel-case', // camelCase
+        'pascal-case', // PascalCase
+        'sentence-case', // Sentence case
+        'start-case', // Start Case
+      ],
     ],
   },
 }
